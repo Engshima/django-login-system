7dbde76f81decf74e68749cb6f56017a320549ce
Engshima <sh.vatanpourasl.com>
2023-11-29 17:04:53 +0330

    edit section is completed

--- login_system/blog/urls.py
+++ login_system/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('',views.home, name='posts'),
     path('about/',views.about, name='about'),
     path('post/create',views.PostCreate, name='PostCreate'),
+    path('post/edit/<int:id>/',views.PostEdit, name='post-edit'),
 ]
 
--- login_system/blog/views.py
+++ login_system/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import *
 from .forms import PostForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -21,14 +23,45 @@ def PostCreate(request):
             context = {'form': PostForm()}
             return render(request, 'blog/post_form.html', context)
       
-      if request.method == 'POST':
+      elif request.method == 'POST':
             form = PostForm(request.POST)
             if form.is_valid():
                 form.save()
+                messages.success(request,"the post has been created successfully")
                 return redirect('blog:posts')
             else:
-                  return render(request,'blog/post_form.html',{'form':form})
+                  
+                return render(request,'blog/post_form.html',{'form':form})
+                messages.error(request, 'Please correct the following errors:')
+          
+           
 
+def PostEdit(request,id):
+      post = get_object_or_404(Post,id=id)
+      if request.method=='GET':
+            form = PostForm(instance=post)
+            context = {
+                  'form': form,
+                  'id' :  id,
+            }
+            return render (request,'blog/post_form.html',context)
+      
+      elif request.method =='POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                messages.success(request,"The post has been updated successfully.")
+                return redirect('blog:posts')
+                
+            else:
+                  messages.error(request,'Please correct the following errors:')
+                  return render (request,'blog/post_form.html',{'form':form})
+            
+                  
+          
+     
+
+     
 def about(request):
       return render(request,'blog/about.html')
     
